7ed7a211e50fecc0d8d8cafa75174dc99e8f6d46
Rohit Naik <techrohitnaik@outlook.com>
2026-06-08 21:17:59 +0530

    Fixed build issue.

--- ThrustSync.Data/Migrations/20260605000002_AddHmcFacToWorkOrder.Designer.cs
+++ ThrustSync.Data/Migrations/20260605000002_AddHmcFacToWorkOrder.Designer.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using ThrustSync.Data.Context;
 using TrustSync.Data;
 
 #nullable disable
